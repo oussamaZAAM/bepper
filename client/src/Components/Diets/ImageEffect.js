1ae2ab517cfae3914dc705f97e6f8c53b22cd097
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-03 22:08:35 +0000

    Add image url if exists, and standarized one if not

--- client/src/Components/Diets/ImageEffect.js
+++ client/src/Components/Diets/ImageEffect.js
@@ -3,11 +3,15 @@ import React from 'react'
 import "./ImageEffect.css"
 
 const ImageEffect = (props) => {
+  console.log(props)
    
   return (
     <div className="flex-center cont_photo">
         <div className="flex-center cont_img_back">
-            <img src={'https://webknox.com/recipeImages/'+props.meal.id+'-556x370.jpg'} alt="" />
+            {props.meal.url 
+              ? <img src={props.meal.url} alt="" />
+              : <img src={'https://webknox.com/recipeImages/'+props.meal.url+'-556x370.jpg'} alt="" />
+            }
         </div>
     </div>
   )
