0fb737c3ebf4d5ed3517867b9dc4bb1e201e45ad
oussamaZAAM <zaam.oussama@gmail.com>
2023-04-03 22:25:55 +0000

    Add Content to Home Page

--- client/src/Components/Main/Main.js
+++ client/src/Components/Main/Main.js
@@ -1,90 +1,143 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { Button } from '@mui/material'
+import { Button } from "@mui/material";
 
-import NutritionCalc from '../NutritionCalc/NutritionCalc'
-import './Main.css'
+import NutritionCalc from "../NutritionCalc/NutritionCalc";
+import "./Main.css";
 
 const Main = () => {
-
-    const existingCalories = localStorage.getItem('calories');
-    const [isDietExist, setIsDietExist] = useState(existingCalories);
-    const navigate = useNavigate();
-    const changeDiet = () => {
-        localStorage.removeItem('calories')
-        localStorage.removeItem('breakfast')
-        localStorage.removeItem('lunch')
-        localStorage.removeItem('dinner')
-        setIsDietExist(false)
-    }
+  const existingCalories = localStorage.getItem("calories");
+  const [isDietExist, setIsDietExist] = useState(existingCalories);
+  const navigate = useNavigate();
+  const changeDiet = () => {
+    localStorage.removeItem("calories");
+    localStorage.removeItem("breakfast");
+    localStorage.removeItem("lunch");
+    localStorage.removeItem("dinner");
+    setIsDietExist(false);
+  };
   return (
-    <div className='container main-position'>
-        <div className='container flex-center bg-container title-text '>
-            <div className='container flex-center row'>
-                <div className='container row flex-center text-center'>
-                    <div className='col-12'>
-                        <h1 className='title'>Your Way to Control Your Diet</h1>
-                    </div>
-                    <div className='col-12 description'>
-                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque lobortis rhoncus. Proin luctus, 
-                            erat ut pharetra porttitor, est sem vehicula nibh, sed ullamcorper nisl risus vitae est. Donec blandit leo ligula.
-                            Suspendisse quis tellus eu neque blandit ullamcorper et elementum quam. Fusce ac porta dolor, non dapibus arcu. 
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tempus ullamcorper nulla et dictum.</p>
-                    </div>
-                </div>
+    <div className="container main-position">
+      <div className="container flex-center bg-container title-text ">
+        <div className="container flex-center row">
+          <div className="container row flex-center text-center">
+            <div className="col-12">
+              <h1 className="title">Your Way to Control Your Diet</h1>
             </div>
-        </div>
-        <div className='calories-calc'>
-
-        {!isDietExist 
-            ? <div className='container flex-center'>
-                <NutritionCalc />
+            <div className="col-12 description">
+              <p>
+                Welcome to our Bepper website! We're here to help you achieve
+                your health and fitness goals by providing you with the tools
+                and resources you need to plan your meals and track your food
+                intake. We believe that healthy eating should be enjoyable and
+                sustainable, which is why we offer a variety of recipes that are
+                both delicious and nutritious.
+                <br />
+                Whether you're looking for quick and easy meals or more
+                elaborate dishes for special occasions, we have something for
+                everyone.
+              </p>
             </div>
-            : <div className='container flex-center row'>
-                <div className='container row flex-center text-center'>
-                    <div className='col-12'>
-                        <h1 className='title'>Your daily Diet</h1>
-                    </div>
-                    <h4 className='title'><span className='calories'>{existingCalories}</span> calories per day.</h4>
-                    <div className='container row flex-column-css'>
-                        <Button onClick={()=>navigate('/diets')} variant='outlined' className='m-2 col-sm-12 col-md-6 col-lg-4' color='success'>Check your Diet</Button>
-                        <Button onClick={changeDiet} variant='contained' className='m-2 col-sm-12 col-md-6 col-lg-4' color='success'>Change your daily Diet</Button>
-                    </div>
-                </div>
+          </div>
+        </div>
+      </div>
+      <div className="calories-calc">
+        {!isDietExist ? (
+          <div className="container flex-center">
+            <NutritionCalc />
+          </div>
+        ) : (
+          <div className="container flex-center row">
+            <div className="container row flex-center text-center">
+              <div className="col-12">
+                <h1 className="title">Your daily Diet</h1>
+              </div>
+              <h4 className="title">
+                <span className="calories">{existingCalories}</span> calories
+                per day.
+              </h4>
+              <div className="container row flex-column-css">
+                <Button
+                  onClick={() => navigate("/diets")}
+                  variant="outlined"
+                  className="m-2 col-sm-12 col-md-6 col-lg-4"
+                  color="success"
+                >
+                  Check your Diet
+                </Button>
+                <Button
+                  onClick={changeDiet}
+                  variant="contained"
+                  className="m-2 col-sm-12 col-md-6 col-lg-4"
+                  color="success"
+                >
+                  Change your daily Diet
+                </Button>
+              </div>
             </div>
-        }
-
+          </div>
+        )}
+      </div>
+      <div className="container app-desc flex-column-css">
+        <div className="row">
+          <h1 className="col-12 title col-12">Bell Pepper</h1>
         </div>
-        <div className='container app-desc flex-column-css'>
-                <div className='row'><h1 className='col-12 title col-12'>Bell Pepper</h1></div>
-                <div className='row mt-5'>
-                    <div className='col-sm-12 col-md-6 app-desciptions flex-column-css'>
-                        <img alt='normal-logo' className='logo-size' src={require("../../Logos/logo/logo-green.png")} />
-                        <h5>Schedule Your Day's Food</h5>
-                        <p className='desc-textarea'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque lobortis rhoncus. Proin luctus</p>
-                    </div>
-                    <div className='col-sm-12 col-md-6 app-desciptions flex-column-css'>
-                        <img alt='normal-logo' className='logo-size' src={require("../../Logos/logo/logo-green.png")} />
-                        <h5>Schedule Your Day's Food</h5>
-                        <p className='desc-textarea'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque lobortis rhoncus. Proin luctus</p>
-                    </div>
-                </div>
-                <div className='row mt-5'>
-                    <div className='col-sm-12 col-md-6 app-desciptions flex-column-css'>
-                        <img alt='normal-logo' className='logo-size' src={require("../../Logos/logo/logo-green.png")} />
-                        <h5>Schedule Your Day's Food</h5>
-                        <p className='desc-textarea'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque lobortis rhoncus. Proin luctus</p>
-                    </div>
-                    <div className='col-sm-12 col-md-6 app-desciptions flex-column-css'>
-                        <img alt='normal-logo' className='logo-size' src={require("../../Logos/logo/logo-green.png")} />
-                        <h5>Schedule Your Day's Food</h5>
-                        <p className='desc-textarea'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus scelerisque lobortis rhoncus. Proin luctus</p>
-                    </div>
-                </div>
+        <div className="row mt-5">
+          <div className="col-sm-12 col-md-6 app-desciptions flex-column-css">
+            <img
+              alt="normal-logo"
+              className="logo-size"
+              src={require("../../Logos/logo/logo-green.png")}
+            />
+            <h5 className="title">Calculate your daily intake</h5>
+            <p className="desc-textarea">
+              Calculate the daily calories intake to ensure you're staying on
+              track with your nutrition goals.
+            </p>
+          </div>
+          <div className="col-sm-12 col-md-6 app-desciptions flex-column-css">
+            <img
+              alt="normal-logo"
+              className="logo-size"
+              src={require("../../Logos/logo/logo-green.png")}
+            />
+            <h5 className="title">Plan your daily meals</h5>
+            <p className="desc-textarea">
+              Our meal planner tool is designed to make meal planning simple and
+              easy.
+            </p>
+          </div>
         </div>
+        <div className="row mt-5">
+          <div className="col-sm-12 col-md-6 app-desciptions flex-column-css">
+            <img
+              alt="normal-logo"
+              className="logo-size"
+              src={require("../../Logos/logo/logo-green.png")}
+            />
+            <h5 className="title">Customize your meal</h5>
+            <p className="desc-textarea">
+              While we offer you a variety of meals based on the daily need of
+              calories, you still have the option to customize your daily meal
+              by changing the meals you don't like
+            </p>
+          </div>
+          <div className="col-sm-12 col-md-6 app-desciptions flex-column-css">
+            <img
+              alt="normal-logo"
+              className="logo-size"
+              src={require("../../Logos/logo/logo-green.png")}
+            />
+            <h5 className="title">Track and save your meals</h5>
+            <p className="desc-textarea">
+                We also offer a food tracking feature that allows you to log your daily food intake and adjust your saved meals.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
